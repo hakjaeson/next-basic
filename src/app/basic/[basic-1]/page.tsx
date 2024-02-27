ff0c3bf968bb358508cd573b7a86cc623292256b
hakjaeson <hakjae.son@gmail.com>
2024-02-27 12:48:59 +0900

    next-basic-1

---
+++ src/app/basic/[basic-1]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  console.log("console test");
+  return (
+    <div>
+      <div>basic/basic-1 page</div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>test page</div>
+    </div>
+  );
+};
+
+export default page;
